2ae29dc16a5d7e1071d36e6418decaab37ff5564
Asifuddoula Sakib <asifuddoula76@gmail.com>
2022-10-24 22:56:35 +0600

    git update Mon Oct 24 22:56:34 BST 2022

--- app/Models/Bank.php
+++ app/Models/Bank.php
@@ -7,29 +7,33 @@ use Illuminate\Database\Eloquent\Model;
 
 class Bank extends Model
 {
-    use HasFactory;
+  use HasFactory;
 
-    protected $fillable = [
-        'account_no',
-        'check_no',
-        'amount',
-        'cost_status',
-    ];
+  protected $fillable = [
+    'bank_name',
+    'account_name',
+    'account_no',
+    'check_no',
+    'amount',
+    'issue_date',
+    'cost_status',
+    'description',
+  ];
 
-    protected $appends = ['view_route', 'edit_route', 'delete_route'];
+  protected $appends = ['view_route', 'edit_route', 'delete_route'];
 
-    public function setViewRouteAttribute()
-    {
-        return 'bank.view';
-    }
+  public function setViewRouteAttribute()
+  {
+    return 'bank.view';
+  }
 
-    public function setEditRouteAttribute()
-    {
-        return 'admin.bank.edit';
-    }
+  public function setEditRouteAttribute()
+  {
+    return 'admin.bank.edit';
+  }
 
-    public function setDeleteRouteAttribute()
-    {
-        return 'admin.bank.delete';
-    }
+  public function setDeleteRouteAttribute()
+  {
+    return 'admin.bank.delete';
+  }
 }
--- resources/views/admin/bank/edit.blade.php
+++ resources/views/admin/bank/edit.blade.php
@@ -3,8 +3,8 @@
 @section('css')
     <style>
         /* .container-fluid {
-            background-color: red;
-        } */
+                        background-color: red;
+                    } */
     </style>
 @endsection
 
@@ -18,14 +18,14 @@
 
     <div class="card">
         <div class="card-body">
-            <form method="post" action="{{ route('admin.bank.store') }}">
+            <form method="post" action="{{ route('admin.bank.update', $bank->id) }}">
                 @csrf
                 <div class="row">
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="bank_name" class="form-label">Bank Name</label>
                             <input type="text" name="bank_name" class="form-control" id="bank_name"
-                                placeholder="Bank Name">
+                                placeholder="Bank Name" value="{{ $bank->bank_name }}">
                             @error('bank_name')
                                 <div id="bank_name" class="form-text text-danger">{{ $message }}</div>
                             @enderror
@@ -35,7 +35,7 @@
                         <div class="form-group">
                             <label for="account_name" class="form-label">Account Name</label>
                             <input type="text" name="account_name" class="form-control" id="account_name"
-                                placeholder="Account Name">
+                                placeholder="Account Name" value="{{ $bank->account_name }}">
                             @error('account_name')
                                 <div id="account_name" class="form-text text-danger">{{ $message }}</div>
                             @enderror
@@ -45,7 +45,7 @@
                         <div class="form-group">
                             <label for="account_no" class="form-label">Account No <span class="text-danger">*</span></label>
                             <input type="text" name="account_no" class="form-control" id="account_no"
-                                placeholder="Account No">
+                                placeholder="Account No" value="{{ $bank->account_no }}">
                             @error('account_no')
                                 <div id="account_no" class="form-text text-danger">{{ $message }}</div>
                             @enderror
@@ -54,8 +54,8 @@
                     <div class="col-md-6">
                         <div class="form-group">
                             <label for="check_no" class="form-label">Check No <span class="text-danger">*</span></label>
-                            <input type="text" name="check_no" class="form-control" id="check_no"
-                                placeholder="Check No">
+                            <input type="text" name="check_no" class="form-control" id="check_no" placeholder="Check No"
+                                value="{{ $bank->check_no }}">
                             @error('check_no')
                                 <div id="check_no" class="form-text text-danger">{{ $message }}</div>
                             @enderror
@@ -64,7 +64,8 @@
                     <div class="col-md-4">
                         <div class="form-group">
                             <label for="amount" class="form-label">Amount <span class="text-danger">*</span></label>
-                            <input type="number" name="amount" class="form-control" id="amount" placeholder="Amount">
+                            <input type="number" name="amount" class="form-control" id="amount" placeholder="Amount"
+                                value="{{ $bank->amount }}">
                             @error('amount')
                                 <div id="amount" class="form-text text-danger">{{ $message }}</div>
                             @enderror
@@ -74,7 +75,7 @@
                         <div class="form-group">
                             <label for="issue_date" class="form-label">Issue Date <span class="text-danger">*</span></label>
                             <input type="date" name="issue_date" class="form-control" id="issue_date"
-                                placeholder="issue_date">
+                                placeholder="issue_date" value="{{ $bank->issue_date }}">
                             @error('issue_date')
                                 <div id="issue_date" class="form-text text-danger">{{ $message }}</div>
                             @enderror
@@ -86,8 +87,8 @@
                                     class="text-danger">*</span></label>
                             <select class="form-control" name="cost_status" id="cost_status">
                                 <option value="">Select Cost Status</option>
-                                <option value="1">Debit</option>
-                                <option value="2">Cradit</option>
+                                <option value="1" {{ $bank->cost_status == 1 ? 'selected' : '' }}>Debit</option>
+                                <option value="2" {{ $bank->cost_status == 2 ? 'selected' : '' }}>Cradit</option>
                             </select>
                             @error('cost_status')
                                 <div id="cost_status" class="form-text text-danger">{{ $message }}</div>
@@ -97,7 +98,7 @@
                     <div class="col-md-12">
                         <div class="form-group">
                             <label for="cost_status" class="form-label">Description</label>
-                            <textarea class="form-control" name="description" id="description" cols="30" rows="5"></textarea>
+                            <textarea class="form-control" name="description" id="description" cols="30" rows="5">{{ $bank->description }}</textarea>
                             @error('description')
                                 <div id="description" class="form-text text-danger">{{ $message }}</div>
                             @enderror
